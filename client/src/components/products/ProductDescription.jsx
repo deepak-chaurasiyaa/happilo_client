2c58f4988bc01c4f23cef97805bbb69202efbe33
Deepak_Chaurasiya <dchaurasiya8589@gmail.com>
2023-03-27 15:05:06 +0530

    description page description text and button work

--- client/src/components/products/ProductDescription.jsx
+++ client/src/components/products/ProductDescription.jsx
@@ -1,83 +1,35 @@
-import { Box } from '@mui/system';
 import React, { useState } from 'react';
-import Header from '../header/Header';
 
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-const itemData = [
-  {
-    img: 'https://cdn.shopify.com/s/files/1/0569/6867/5527/products/1v2_bf9e06f6-cd22-4209-a932-310a869534c0_720x.png?v=1632846581',
-    title: 'Breakfast',
-    author: '@bkristastucchio',
+import {
+  Box,
+  Button,
+  Typography,
+  ImageList,
+  ImageListItem,
+} from '@mui/material';
+
+import styled from '@emotion/styled';
+
+import Header from '../header/Header';
+import { availableSizes, itemData } from '../../shared/constant';
+
+const ColorButton = styled(Button)(({ theme }) => ({
+  padding: '-20px !important',
+  '&:hover': {
+    backgroundColor: '#00523b',
+    color: 'white',
   },
-  {
-    img: 'https://cdn.shopify.com/s/files/1/0569/6867/5527/products/back_fba9f5e6-686e-4e5b-aad9-31fffe66be68_900x.png?v=1632846581',
-    title: 'Burger',
-    author: '@rollelflex_graphy726',
-  },
-  {
-    img: 'https://cdn.shopify.com/s/files/1/0569/6867/5527/products/4v2_8e1bdd96-edf1-4662-9059-9ff3bb5575da_900x.jpg?v=1632846580',
-    title: 'Camera',
-    author: '@helloimnik',
-  },
-  {
-    img: 'https://cdn.shopify.com/s/files/1/0569/6867/5527/products/3_ff429d5d-d39f-4917-8288-6c2ed49a4b24_900x.jpg?v=1632846582',
-    title: 'Coffee',
-    author: '@nolanissac',
-  },
-  {
-    img: 'https://cdn.shopify.com/s/files/1/0569/6867/5527/products/5_145bf845-995e-4039-b52a-3cd15e6ec359_900x.jpg?v=1632846582',
-    title: 'Hats',
-    author: '@hjrc33',
-  },
-  {
-    img: 'https://cdn.shopify.com/s/files/1/0569/6867/5527/products/6_46cf0eb5-4ca8-42b6-854d-951ae338df56_900x.jpg?v=1632846582',
-    title: 'Honey',
-    author: '@arwinneil',
-  },
-  {
-    img: 'https://cdn.shopify.com/s/files/1/0569/6867/5527/products/7_a82ecf2c-2e9f-4983-840e-2821633c1007_900x.jpg?v=1632846581',
-    title: 'Basketball',
-    author: '@tjdragotta',
-  },
-  {
-    img: 'https://cdn.shopify.com/s/files/1/0569/6867/5527/products/packof2_3bd63f6e-ef0b-49d0-932f-37e8441b0d21_900x.jpg?v=1632846670',
-    title: 'Fern',
-    author: '@katie_wasserman',
-  },
-  {
-    img: 'https://cdn.shopify.com/s/files/1/0569/6867/5527/products/packof5_48baaae3-0970-4b46-849c-4f770d758e14_900x.jpg?v=1632846698',
-    title: 'Mushrooms',
-    author: '@silverdalex',
-  },
-  {
-    img: 'https://cdn.shopify.com/s/files/1/0569/6867/5527/products/1v2_8fa71dc0-6ea4-4663-8967-c6049ad5da62_900x.png?v=1632846639',
-    title: 'Tomato basil',
-    author: '@shelleypauls',
-  },
-  {
-    img: 'https://cdn.shopify.com/s/files/1/0569/6867/5527/products/almondspackof4_900x.jpg?v=1679307809',
-    title: 'Sea star',
-    author: '@peterlaster',
-  },
-  {
-    img: 'https://cdn.shopify.com/s/files/1/0569/6867/5527/products/610wodrIz_L._SL1000_39113a24-b7e9-4a06-bd17-88cd4bc8e520_900x.jpg?v=1679307809',
-    title: 'Bike',
-    author: '@southside_customs',
-  },
-  {
-    img: 'https://cdn.shopify.com/s/files/1/0569/6867/5527/products/packof2_cd522605-0e54-483a-b6bf-99c5cc6618d9_900x.jpg?v=1679307623',
-    title: 'Bike',
-    author: '@southside_customs',
-  },
-];
+}));
 
 const ProductDescription = () => {
   const [currentImage, setCurrentImage] = useState({ ...itemData[0] });
+  const [currentPackSize, setCurrentPackSize] = useState({
+    ...availableSizes[0],
+  });
   return (
     <>
       <Header />
-      <Box sx={{ padding: '10rem 0 0 2rem', display: 'flex' }}>
+      <Box sx={{ padding: '12rem 0 0 2rem', display: 'flex' }}>
         <Box>
           <ImageList sx={{ width: 130, height: 500 }} cols={1}>
             {itemData.map((item) => (
@@ -97,23 +49,47 @@ const ProductDescription = () => {
             ))}
           </ImageList>
         </Box>
-        <Box>
-          <ImageList sx={{ width: '90%', margin: 'auto' }}>
-            <ImageListItem>
-              <img
-                className='description-image'
-                style={{
-                  borderRadius: '15px',
-                  backgroundColor: 'white',
-                  width: '70vh',
-                }}
-                src={currentImage.img}
-                alt=''
-              />
-            </ImageListItem>
-          </ImageList>
+        <Box sx={{ padding: ' 0 3rem' }}>
+          <img
+            className='description-image'
+            style={{
+              borderRadius: '15px',
+              backgroundColor: 'white',
+              width: '30rem',
+            }}
+            src={currentImage.img}
+            alt=''
+          />
         </Box>
-        <Box sx={{ width: '35%' }}>Hello</Box>
+        <Box>
+          <Typography variant='h6' sx={{ fontWeight: 600 }}>
+            Happilo 100% Natural Premium California Almonds
+          </Typography>
+          <Typography variant='h6' sx={{ fontWeight: 600 }}>
+            ₹ {currentPackSize.price.toFixed(2)}
+          </Typography>
+          <br />
+          <Box sx={{ display: 'flex' }}>
+            <Typography sx={{ fontWeight: 600 }}>Size </Typography>
+            <Typography sx={{ padding: '0px 10px 10px 10px' }}>
+              {currentPackSize.quantity}
+            </Typography>
+          </Box>
+          <Box sx={{ width: '40rem' }}>
+            {availableSizes.map((product) => {
+              return (
+                <ColorButton
+                  onClick={() => setCurrentPackSize(product)}
+                  style={{ textTransform: 'none' }}
+                  sx={{ margin: '0 6px 6px 6px', backgroundColor: 'white' }}
+                  variant='contained'
+                >
+                  {product.quantity}
+                </ColorButton>
+              );
+            })}
+          </Box>
+        </Box>
       </Box>
     </>
   );
--- client/src/shared/constant.js
+++ client/src/shared/constant.js
@@ -160,5 +160,95 @@ export const PRODUCT_LABEL_BACKGROUND = {
 };
 
 export const FOOTER_TEXT = {
-  COPYRIGHT_MESSAGE: '© 2023 Happilo_Clone International PVT. LTD. All Rights Reserved.(Deepak & Vishal)',
+  COPYRIGHT_MESSAGE:
+    '© 2023 Happilo_Clone International PVT. LTD. All Rights Reserved.(Deepak & Vishal)',
 };
+
+export const availableSizes = [
+  { id: 1, quantity: '200g', price: 203 },
+  { id: 2, quantity: '200g (Pack of 2)', price: 399 },
+  { id: 3, quantity: '200g Pack of 5', price: 899.5 },
+  { id: 4, quantity: '500g', price: 820.5 },
+  {
+    id: 5,
+    quantity: '500g (Pack of 2)',
+    price: 449,
+  },
+  {
+    id: 6,
+    quantity: '1kg',
+    price: 799,
+  },
+  {
+    id: 7,
+    quantity: '1kg (Pack of 2)',
+    price: 1550,
+  },
+];
+export const itemData = [
+  {
+    img: 'https://cdn.shopify.com/s/files/1/0569/6867/5527/products/1v2_bf9e06f6-cd22-4209-a932-310a869534c0_720x.png?v=1632846581',
+    title: 'Breakfast',
+    author: '@bkristastucchio',
+  },
+  {
+    img: 'https://cdn.shopify.com/s/files/1/0569/6867/5527/products/back_fba9f5e6-686e-4e5b-aad9-31fffe66be68_900x.png?v=1632846581',
+    title: 'Burger',
+    author: '@rollelflex_graphy726',
+  },
+  {
+    img: 'https://cdn.shopify.com/s/files/1/0569/6867/5527/products/4v2_8e1bdd96-edf1-4662-9059-9ff3bb5575da_900x.jpg?v=1632846580',
+    title: 'Camera',
+    author: '@helloimnik',
+  },
+  {
+    img: 'https://cdn.shopify.com/s/files/1/0569/6867/5527/products/3_ff429d5d-d39f-4917-8288-6c2ed49a4b24_900x.jpg?v=1632846582',
+    title: 'Coffee',
+    author: '@nolanissac',
+  },
+  {
+    img: 'https://cdn.shopify.com/s/files/1/0569/6867/5527/products/5_145bf845-995e-4039-b52a-3cd15e6ec359_900x.jpg?v=1632846582',
+    title: 'Hats',
+    author: '@hjrc33',
+  },
+  {
+    img: 'https://cdn.shopify.com/s/files/1/0569/6867/5527/products/6_46cf0eb5-4ca8-42b6-854d-951ae338df56_900x.jpg?v=1632846582',
+    title: 'Honey',
+    author: '@arwinneil',
+  },
+  {
+    img: 'https://cdn.shopify.com/s/files/1/0569/6867/5527/products/7_a82ecf2c-2e9f-4983-840e-2821633c1007_900x.jpg?v=1632846581',
+    title: 'Basketball',
+    author: '@tjdragotta',
+  },
+  {
+    img: 'https://cdn.shopify.com/s/files/1/0569/6867/5527/products/packof2_3bd63f6e-ef0b-49d0-932f-37e8441b0d21_900x.jpg?v=1632846670',
+    title: 'Fern',
+    author: '@katie_wasserman',
+  },
+  {
+    img: 'https://cdn.shopify.com/s/files/1/0569/6867/5527/products/packof5_48baaae3-0970-4b46-849c-4f770d758e14_900x.jpg?v=1632846698',
+    title: 'Mushrooms',
+    author: '@silverdalex',
+  },
+  {
+    img: 'https://cdn.shopify.com/s/files/1/0569/6867/5527/products/1v2_8fa71dc0-6ea4-4663-8967-c6049ad5da62_900x.png?v=1632846639',
+    title: 'Tomato basil',
+    author: '@shelleypauls',
+  },
+  {
+    img: 'https://cdn.shopify.com/s/files/1/0569/6867/5527/products/almondspackof4_900x.jpg?v=1679307809',
+    title: 'Sea star',
+    author: '@peterlaster',
+  },
+  {
+    img: 'https://cdn.shopify.com/s/files/1/0569/6867/5527/products/610wodrIz_L._SL1000_39113a24-b7e9-4a06-bd17-88cd4bc8e520_900x.jpg?v=1679307809',
+    title: 'Bike',
+    author: '@southside_customs',
+  },
+  {
+    img: 'https://cdn.shopify.com/s/files/1/0569/6867/5527/products/packof2_cd522605-0e54-483a-b6bf-99c5cc6618d9_900x.jpg?v=1679307623',
+    title: 'Bike',
+    author: '@southside_customs',
+  },
+];
