32fea1a4da33b58b6055d9b65909234b63ee77b2
Deepak_Chaurasiya <dchaurasiya8589@gmail.com>
2023-03-29 02:34:33 +0530

    1 footer section screenshots added

--- client/src/components/happiloJourney/Journey.jsx
+++ client/src/components/happiloJourney/Journey.jsx
@@ -13,102 +13,114 @@ const JourneyPage = () => {
       sx={{
         width: '97%',
         margin: '1rem auto',
-        backgroundColor: '#f0e7f6',
-        paddingTop: '1.5rem',
-        paddingBottom: '1.5rem',
       }}
     >
-      <Box>
-        <Typography variant='h4' align='center' className='journey'>
-          HAPPILO’S JOURNEY
-        </Typography>
-      </Box>
-      <PaperJourney className='journey-mid'>
+      <Box
+        sx={{
+          backgroundColor: '#f0e7f6',
+          paddingTop: '1.5rem',
+          paddingBottom: '1.5rem',
+        }}
+      >
         <Box>
-          <iframe
-            className='journey-video-container'
-            src='https://www.youtube.com/embed/Mh7yQNcSbbE?controls=0'
-            title='YouTube video player'
-            frameborder='0'
-            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-            allowfullscreen
-          ></iframe>
-        </Box>
-        <Box
-          className='journey-textContainer'
-          style={{ border: '1px solid #3e3d3d6b' }}
-        >
-          <Typography variant='body1' className='journey-text'>
-            {JOURNEY_TEXT.JOURNEY_MESSAGE}
+          <Typography variant='h4' align='center' className='journey'>
+            HAPPILO’S JOURNEY
           </Typography>
-          <button className='journey-button'>SHOP NOW</button>
-          <img
-            style={{
-              width: 80,
-              height: 79,
-              position: 'relative',
-
-              bottom: '24rem',
-              left: '15rem',
-            }}
-            alt=''
-            src='https://cdn.shopify.com/s/files/1/0569/6867/5527/files/nut-emoji.png?v=1671441267'
-            loading='lazy'
-          ></img>
-          <img
-            style={{
-              width: 80,
-              height: 83,
-              position: 'relative',
-
-              bottom: '22rem',
-              left: '26rem',
-            }}
-            alt=''
-            src='https://cdn.shopify.com/s/files/1/0569/6867/5527/files/nut-emoji-1.png?v=1671442683'
-            loading='lazy'
-          ></img>
-          <img
-            style={{
-              width: 80,
-              height: 79,
-              position: 'relative',
-
-              bottom: '2rem',
-              left: '25rem',
-            }}
-            alt=''
-            src='https://cdn.shopify.com/s/files/1/0569/6867/5527/files/nut-emoji-2.png?v=1671442683'
-            loading='lazy'
-          ></img>
-          <img
-            style={{
-              width: 80,
-              height: 79,
-              position: 'relative',
-
-              bottom: '-2rem',
-              left: '15rem',
-            }}
-            alt=''
-            src='https://cdn.shopify.com/s/files/1/0569/6867/5527/files/nut-emoji-3.png?v=1671442684'
-            loading='lazy'
-          ></img>
-          <img
-            style={{
-              width: 80,
-              height: 79,
-              position: 'relative',
-
-              bottom: '-2rem',
-              left: '-16rem',
-            }}
-            alt=''
-            src='	https://cdn.shopify.com/s/files/1/0569/6867/5527/files/nut-emoji-4.png?v=1671442684'
-            loading='lazy'
-          ></img>
         </Box>
-      </PaperJourney>
+        <PaperJourney className='journey-mid'>
+          <Box>
+            <iframe
+              className='journey-video-container'
+              src='https://www.youtube.com/embed/Mh7yQNcSbbE?controls=0'
+              title='YouTube video player'
+              frameborder='0'
+              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+              allowfullscreen
+            ></iframe>
+          </Box>
+          <Box
+            className='journey-textContainer'
+            style={{ border: '1px solid #3e3d3d6b' }}
+          >
+            <Typography variant='body1' className='journey-text'>
+              {JOURNEY_TEXT.JOURNEY_MESSAGE}
+            </Typography>
+            <button className='journey-button'>SHOP NOW</button>
+            <img
+              style={{
+                width: 80,
+                height: 79,
+                position: 'relative',
+
+                bottom: '24rem',
+                left: '15rem',
+              }}
+              alt=''
+              src='https://cdn.shopify.com/s/files/1/0569/6867/5527/files/nut-emoji.png?v=1671441267'
+              loading='lazy'
+            ></img>
+            <img
+              style={{
+                width: 80,
+                height: 83,
+                position: 'relative',
+
+                bottom: '22rem',
+                left: '26rem',
+              }}
+              alt=''
+              src='https://cdn.shopify.com/s/files/1/0569/6867/5527/files/nut-emoji-1.png?v=1671442683'
+              loading='lazy'
+            ></img>
+            <img
+              style={{
+                width: 80,
+                height: 79,
+                position: 'relative',
+
+                bottom: '2rem',
+                left: '25rem',
+              }}
+              alt=''
+              src='https://cdn.shopify.com/s/files/1/0569/6867/5527/files/nut-emoji-2.png?v=1671442683'
+              loading='lazy'
+            ></img>
+            <img
+              style={{
+                width: 80,
+                height: 79,
+                position: 'relative',
+
+                bottom: '-2rem',
+                left: '15rem',
+              }}
+              alt=''
+              src='https://cdn.shopify.com/s/files/1/0569/6867/5527/files/nut-emoji-3.png?v=1671442684'
+              loading='lazy'
+            ></img>
+            <img
+              style={{
+                width: 80,
+                height: 79,
+                position: 'relative',
+
+                bottom: '-2rem',
+                left: '-16rem',
+              }}
+              alt=''
+              src='	https://cdn.shopify.com/s/files/1/0569/6867/5527/files/nut-emoji-4.png?v=1671442684'
+              loading='lazy'
+            ></img>
+          </Box>
+        </PaperJourney>
+      </Box>
+      <Box sx={{ padding: '2rem 0' }}>
+        <img
+          alt=''
+          src='sc_.png'
+          style={{ width: '100%', padding: '20px 0 -20px 0' }}
+        />
+      </Box>
     </Box>
   );
 };
