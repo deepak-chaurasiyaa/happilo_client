136f2b1384958a74b0398493bb02b9a7025cb6d6
Deepak_Chaurasiya <dchaurasiya8589@gmail.com>
2023-03-29 01:59:37 +0530

    Filter Section layout creation

--- client/src/components/productsCollections/ProductView.jsx
+++ client/src/components/productsCollections/ProductView.jsx
@@ -1,6 +1,14 @@
-import React from 'react';
-import { Typography, Box, Toolbar } from '@mui/material';
+import React, { useState } from 'react';
+import {
+  Typography,
+  Box,
+  Toolbar,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+} from '@mui/material';
 
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { Link } from 'react-router-dom';
 
 import ProductCollectionCard from './ProductCollectionCard';
@@ -20,7 +28,436 @@ export default function ProductView({ Product, MainTitle }) {
         </Toolbar>
       </Box>
       <Box className='product-collection-product-view'>
-        <Box className='thiner-box-shadow padding-top-none'>Hello</Box>
+        <Box className='thiner-box-shadow padding-top-none'>
+          <div>
+            <Accordion defaultExpanded>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls='panel1a-content'
+                id='panel1a-header'
+              >
+                <Typography>COLLECTION</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Box>
+                  <Typography variant='span'>
+                    <input type='checkbox' />
+                  </Typography>
+                  <Typography variant='span' padding='0 0 0 5px'>
+                    Home Best Seller
+                  </Typography>
+                  <Typography
+                    variant='span'
+                    padding='0 0 0 5px'
+                    sx={{ float: 'right' }}
+                  >
+                    (5)
+                  </Typography>
+                </Box>
+                <Box>
+                  <Typography variant='span'>
+                    <input type='checkbox' />
+                  </Typography>
+                  <Typography variant='span' padding='0 0 0 5px'>
+                    Home Best Seller
+                  </Typography>
+                  <Typography
+                    variant='span'
+                    padding='0 0 0 5px'
+                    sx={{ float: 'right' }}
+                  >
+                    (5)
+                  </Typography>
+                </Box>
+                <Box>
+                  <Typography variant='span'>
+                    <input type='checkbox' />
+                  </Typography>
+                  <Typography variant='span' padding='0 0 0 5px'>
+                    Home Best Seller
+                  </Typography>
+                  <Typography
+                    variant='span'
+                    padding='0 0 0 5px'
+                    sx={{ float: 'right' }}
+                  >
+                    (5)
+                  </Typography>
+                </Box>
+                <Box>
+                  <Typography variant='span'>
+                    <input type='checkbox' />
+                  </Typography>
+                  <Typography variant='span' padding='0 0 0 5px'>
+                    Home Best Seller
+                  </Typography>
+                  <Typography
+                    variant='span'
+                    padding='0 0 0 5px'
+                    sx={{ float: 'right' }}
+                  >
+                    (5)
+                  </Typography>
+                </Box>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion defaultExpanded>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls='panel2a-content'
+                id='panel2a-header'
+              >
+                <Typography>VENDOR</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Box>
+                  <Typography variant='span'>
+                    <input type='checkbox' />
+                  </Typography>
+                  <Typography variant='span' padding='0 0 0 5px'>
+                    Home Best Seller
+                  </Typography>
+                  <Typography
+                    variant='span'
+                    padding='0 0 0 5px'
+                    sx={{ float: 'right' }}
+                  >
+                    (5)
+                  </Typography>
+                </Box>
+                <Box>
+                  <Typography variant='span'>
+                    <input type='checkbox' />
+                  </Typography>
+                  <Typography variant='span' padding='0 0 0 5px'>
+                    Home Best Seller
+                  </Typography>
+                  <Typography
+                    variant='span'
+                    padding='0 0 0 5px'
+                    sx={{ float: 'right' }}
+                  >
+                    (5)
+                  </Typography>
+                </Box>
+                <Box>
+                  <Typography variant='span'>
+                    <input type='checkbox' />
+                  </Typography>
+                  <Typography variant='span' padding='0 0 0 5px'>
+                    Home Best Seller
+                  </Typography>
+                  <Typography
+                    variant='span'
+                    padding='0 0 0 5px'
+                    sx={{ float: 'right' }}
+                  >
+                    (5)
+                  </Typography>
+                </Box>
+                <Box>
+                  <Typography variant='span'>
+                    <input type='checkbox' />
+                  </Typography>
+                  <Typography variant='span' padding='0 0 0 5px'>
+                    Home Best Seller
+                  </Typography>
+                  <Typography
+                    variant='span'
+                    padding='0 0 0 5px'
+                    sx={{ float: 'right' }}
+                  >
+                    (5)
+                  </Typography>
+                </Box>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion defaultExpanded>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls='panel2a-content'
+                id='panel2a-header'
+              >
+                <Typography>PRICE</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Box>
+                  <Typography variant='span'>
+                    <input type='checkbox' />
+                  </Typography>
+                  <Typography variant='span' padding='0 0 0 5px'>
+                    Home Best Seller
+                  </Typography>
+                  <Typography
+                    variant='span'
+                    padding='0 0 0 5px'
+                    sx={{ float: 'right' }}
+                  >
+                    (5)
+                  </Typography>
+                </Box>
+                <Box>
+                  <Typography variant='span'>
+                    <input type='checkbox' />
+                  </Typography>
+                  <Typography variant='span' padding='0 0 0 5px'>
+                    Home Best Seller
+                  </Typography>
+                  <Typography
+                    variant='span'
+                    padding='0 0 0 5px'
+                    sx={{ float: 'right' }}
+                  >
+                    (5)
+                  </Typography>
+                </Box>
+                <Box>
+                  <Typography variant='span'>
+                    <input type='checkbox' />
+                  </Typography>
+                  <Typography variant='span' padding='0 0 0 5px'>
+                    Home Best Seller
+                  </Typography>
+                  <Typography
+                    variant='span'
+                    padding='0 0 0 5px'
+                    sx={{ float: 'right' }}
+                  >
+                    (5)
+                  </Typography>
+                </Box>
+                <Box>
+                  <Typography variant='span'>
+                    <input type='checkbox' />
+                  </Typography>
+                  <Typography variant='span' padding='0 0 0 5px'>
+                    Home Best Seller
+                  </Typography>
+                  <Typography
+                    variant='span'
+                    padding='0 0 0 5px'
+                    sx={{ float: 'right' }}
+                  >
+                    (5)
+                  </Typography>
+                </Box>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion defaultExpanded>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls='panel2a-content'
+                id='panel2a-header'
+              >
+                <Typography>AVAILABILITY</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Box>
+                  <Typography variant='span'>
+                    <input type='checkbox' />
+                  </Typography>
+                  <Typography variant='span' padding='0 0 0 5px'>
+                    Home Best Seller
+                  </Typography>
+                  <Typography
+                    variant='span'
+                    padding='0 0 0 5px'
+                    sx={{ float: 'right' }}
+                  >
+                    (5)
+                  </Typography>
+                </Box>
+                <Box>
+                  <Typography variant='span'>
+                    <input type='checkbox' />
+                  </Typography>
+                  <Typography variant='span' padding='0 0 0 5px'>
+                    Home Best Seller
+                  </Typography>
+                  <Typography
+                    variant='span'
+                    padding='0 0 0 5px'
+                    sx={{ float: 'right' }}
+                  >
+                    (5)
+                  </Typography>
+                </Box>
+                <Box>
+                  <Typography variant='span'>
+                    <input type='checkbox' />
+                  </Typography>
+                  <Typography variant='span' padding='0 0 0 5px'>
+                    Home Best Seller
+                  </Typography>
+                  <Typography
+                    variant='span'
+                    padding='0 0 0 5px'
+                    sx={{ float: 'right' }}
+                  >
+                    (5)
+                  </Typography>
+                </Box>
+                <Box>
+                  <Typography variant='span'>
+                    <input type='checkbox' />
+                  </Typography>
+                  <Typography variant='span' padding='0 0 0 5px'>
+                    Home Best Seller
+                  </Typography>
+                  <Typography
+                    variant='span'
+                    padding='0 0 0 5px'
+                    sx={{ float: 'right' }}
+                  >
+                    (5)
+                  </Typography>
+                </Box>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion defaultExpanded>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls='panel2a-content'
+                id='panel2a-header'
+              >
+                <Typography>READY TO SHIP</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Box>
+                  <Typography variant='span'>
+                    <input type='checkbox' />
+                  </Typography>
+                  <Typography variant='span' padding='0 0 0 5px'>
+                    Home Best Seller
+                  </Typography>
+                  <Typography
+                    variant='span'
+                    padding='0 0 0 5px'
+                    sx={{ float: 'right' }}
+                  >
+                    (5)
+                  </Typography>
+                </Box>
+                <Box>
+                  <Typography variant='span'>
+                    <input type='checkbox' />
+                  </Typography>
+                  <Typography variant='span' padding='0 0 0 5px'>
+                    Home Best Seller
+                  </Typography>
+                  <Typography
+                    variant='span'
+                    padding='0 0 0 5px'
+                    sx={{ float: 'right' }}
+                  >
+                    (5)
+                  </Typography>
+                </Box>
+                <Box>
+                  <Typography variant='span'>
+                    <input type='checkbox' />
+                  </Typography>
+                  <Typography variant='span' padding='0 0 0 5px'>
+                    Home Best Seller
+                  </Typography>
+                  <Typography
+                    variant='span'
+                    padding='0 0 0 5px'
+                    sx={{ float: 'right' }}
+                  >
+                    (5)
+                  </Typography>
+                </Box>
+                <Box>
+                  <Typography variant='span'>
+                    <input type='checkbox' />
+                  </Typography>
+                  <Typography variant='span' padding='0 0 0 5px'>
+                    Home Best Seller
+                  </Typography>
+                  <Typography
+                    variant='span'
+                    padding='0 0 0 5px'
+                    sx={{ float: 'right' }}
+                  >
+                    (5)
+                  </Typography>
+                </Box>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion defaultExpanded>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls='panel2a-content'
+                id='panel2a-header'
+              >
+                <Typography>SIZE</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Box>
+                  <Typography variant='span'>
+                    <input type='checkbox' />
+                  </Typography>
+                  <Typography variant='span' padding='0 0 0 5px'>
+                    Home Best Seller
+                  </Typography>
+                  <Typography
+                    variant='span'
+                    padding='0 0 0 5px'
+                    sx={{ float: 'right' }}
+                  >
+                    (5)
+                  </Typography>
+                </Box>
+                <Box>
+                  <Typography variant='span'>
+                    <input type='checkbox' />
+                  </Typography>
+                  <Typography variant='span' padding='0 0 0 5px'>
+                    Home Best Seller
+                  </Typography>
+                  <Typography
+                    variant='span'
+                    padding='0 0 0 5px'
+                    sx={{ float: 'right' }}
+                  >
+                    (5)
+                  </Typography>
+                </Box>
+                <Box>
+                  <Typography variant='span'>
+                    <input type='checkbox' />
+                  </Typography>
+                  <Typography variant='span' padding='0 0 0 5px'>
+                    Home Best Seller
+                  </Typography>
+                  <Typography
+                    variant='span'
+                    padding='0 0 0 5px'
+                    sx={{ float: 'right' }}
+                  >
+                    (5)
+                  </Typography>
+                </Box>
+                <Box>
+                  <Typography variant='span'>
+                    <input type='checkbox' />
+                  </Typography>
+                  <Typography variant='span' padding='0 0 0 5px'>
+                    Home Best Seller
+                  </Typography>
+                  <Typography
+                    variant='span'
+                    padding='0 0 0 5px'
+                    sx={{ float: 'right' }}
+                  >
+                    (5)
+                  </Typography>
+                </Box>
+              </AccordionDetails>
+            </Accordion>
+          </div>
+        </Box>
         <Box className='col-size-four'>
           {Product &&
             Product.map((product) => {
