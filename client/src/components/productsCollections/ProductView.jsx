c51b5b897b1d62f739807e075a751aa2cf8bac54
Deepak_Chaurasiya <dchaurasiya8589@gmail.com>
2023-03-29 23:26:24 +0530

    Slider work

--- client/src/components/productsCollections/ProductView.jsx
+++ client/src/components/productsCollections/ProductView.jsx
@@ -6,14 +6,53 @@ import {
   Accordion,
   AccordionSummary,
   AccordionDetails,
+  Slider,
+  Input,
 } from '@mui/material';
 
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { Link } from 'react-router-dom';
 
 import ProductCollectionCard from './ProductCollectionCard';
+function valuetext(value) {
+  return `${1990}°C`;
+}
 
+const minDistance = 10;
 export default function ProductView({ Product, MainTitle }) {
+  const [value1, setValue1] = React.useState([20, 37]);
+
+  const handleChange1 = (event, newValue, activeThumb) => {
+    if (!Array.isArray(newValue)) {
+      return;
+    }
+
+    if (activeThumb === 0) {
+      setValue1([Math.min(newValue[0], value1[1] - minDistance), value1[1]]);
+    } else {
+      setValue1([value1[0], Math.max(newValue[1], value1[0] + minDistance)]);
+    }
+  };
+
+  const [value2, setValue2] = React.useState([20, 37]);
+
+  const handleChange2 = (event, newValue, activeThumb) => {
+    if (!Array.isArray(newValue)) {
+      return;
+    }
+
+    if (newValue[1] - newValue[0] < minDistance) {
+      if (activeThumb === 0) {
+        const clamped = Math.min(newValue[0], 500 - minDistance);
+        setValue2([clamped, clamped + minDistance]);
+      } else {
+        const clamped = Math.max(newValue[1], minDistance);
+        setValue2([clamped - minDistance, clamped]);
+      }
+    } else {
+      setValue2(newValue);
+    }
+  };
   return (
     <Box sx={{ width: '90%', margin: 'auto' }}>
       <Box sx={{ flexGrow: 1 }}>
@@ -181,50 +220,23 @@ export default function ProductView({ Product, MainTitle }) {
                 <Typography>PRICE</Typography>
               </AccordionSummary>
               <AccordionDetails>
-                <Box>
-                  <Typography variant='span'>
-                    <input type='checkbox' />
-                  </Typography>
-                  <Typography variant='span' padding='0 0 0 5px'>
-                    Home Best Seller
-                  </Typography>
-                  <Typography
-                    variant='span'
-                    padding='0 0 0 5px'
-                    sx={{ float: 'right' }}
-                  >
-                    (5)
-                  </Typography>
+                <Box >
+                  <Input className='price-filter-input' type='number'></Input>
+                  <Input className='price-filter-input' type='number'></Input>
                 </Box>
+                <Box></Box>
                 <Box>
-                  <Typography variant='span'>
-                    <input type='checkbox' />
-                  </Typography>
-                  <Typography variant='span' padding='0 0 0 5px'>
-                    Home Best Seller
-                  </Typography>
-                  <Typography
-                    variant='span'
-                    padding='0 0 0 5px'
-                    sx={{ float: 'right' }}
-                  >
-                    (5)
-                  </Typography>
-                </Box>
-                <Box>
-                  <Typography variant='span'>
-                    <input type='checkbox' />
-                  </Typography>
-                  <Typography variant='span' padding='0 0 0 5px'>
-                    Home Best Seller
-                  </Typography>
-                  <Typography
-                    variant='span'
-                    padding='0 0 0 5px'
-                    sx={{ float: 'right' }}
-                  >
-                    (5)
-                  </Typography>
+                  <Slider
+                    getAriaLabel={() => 'Minimum distance'}
+                    value={value1}
+                    onChange={handleChange1}
+                    valueLabelDisplay='auto'
+                    getAriaValueText={valuetext}
+                    disableSwap
+                    min={2}
+                    step={10}
+                    max={300}
+                  />
                 </Box>
                 <Box>
                   <Typography variant='span'>
