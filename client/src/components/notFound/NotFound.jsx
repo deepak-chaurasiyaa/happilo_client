ff94ab694b1e20af89f1059e11d9333e13b9becb
Deepak_Chaurasiya <dchaurasiya8589@gmail.com>
2023-03-31 17:50:21 +0530

    Removed unnecessary dependency

--- client/src/components/notFound/NotFound.jsx
+++ client/src/components/notFound/NotFound.jsx
@@ -1,35 +1,36 @@
-import React from "react";
-import { Typography, Button } from "@material-ui/core";
-import { Link } from "react-router-dom";
+import { Button, Typography } from '@mui/material';
+import React from 'react';
+
+import { Link } from 'react-router-dom';
 
 const styles = {
   container: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    height: "100vh",
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
   },
   title: {
-    fontSize: "4rem",
-    fontWeight: "bold",
-    marginBottom: "2rem",
-    textAlign: "center",
+    fontSize: '4rem',
+    fontWeight: 'bold',
+    marginBottom: '2rem',
+    textAlign: 'center',
   },
   image: {
-    width: "80%",
-    maxWidth: "600px",
-    marginBottom: "2rem",
+    width: '80%',
+    maxWidth: '600px',
+    marginBottom: '2rem',
   },
   button: {
-    marginTop: "2rem",
-    padding: "1rem 2rem",
-    borderRadius: "2rem",
-    fontWeight: "bold",
-    backgroundColor: "#3f51b5",
-    color: "white",
-    "&:hover": {
-      backgroundColor: "#2c387e",
+    marginTop: '2rem',
+    padding: '1rem 2rem',
+    borderRadius: '2rem',
+    fontWeight: 'bold',
+    backgroundColor: '#3f51b5',
+    color: 'white',
+    '&:hover': {
+      backgroundColor: '#2c387e',
     },
   },
 };
@@ -37,16 +38,11 @@ const styles = {
 const NotFound = () => {
   return (
     <div style={styles.container}>
-      <Typography variant="h1" style={styles.title}>
+      <Typography variant='h1' style={styles.title}>
         Oops! Page not found.
       </Typography>
-      <img src='/error404page.gif' alt="not found" style={styles.image} />
-      <Button
-        variant="contained"
-        component={Link}
-        to="/"
-        style={styles.button}
-      >
+      <img src='/error404page.gif' alt='not found' style={styles.image} />
+      <Button variant='contained' component={Link} to='/' style={styles.button}>
         Go back to homepage
       </Button>
     </div>
