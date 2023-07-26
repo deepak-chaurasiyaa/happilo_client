8ee89530bb59448cc931ecae86ad39748b00be30
Deepak Chaurasiya <86410087+deepak-chaurasiyaa@users.noreply.github.com>
2023-07-27 00:39:24 +0530

    Delete server directory

--- server/models/index.js
+++
@@ -1,4 +0,0 @@
-import productModel from './product.model.js';
-import userModel from './user.model.js';
-
-export {productModel,userModel}
--- server/shared/helper/common.js
+++
@@ -1,4 +0,0 @@
-export const isArray = (element) => Array.isArray(element);
-
-export const isEmptyArray = (element) =>
-  !(isArray(element) && element.length > 0);
