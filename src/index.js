d69a3b0df1a186be697973a292a68f7dd954cc31
Deepak Chaurasiya <dchaurasiya8589@gmail.com>
2023-08-03 22:59:45 +0530

    Modified -2

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { store, persistor } from './store/store';
 import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './redux/store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; // you can choose different storage mechanisms, such as local storage or session storage
-import { rootReducer } from '../redux/reducer/root.reducer';
+import { rootReducer } from '../reducer/root.reducer';
 
 const persistConfig = {
   key: 'root', // key used to access the persisted state in storage
